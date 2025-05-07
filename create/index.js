ffdc63ec778416ac84950a0dfb61746afbc418d9
denis <gurami152@gmail.com>
2025-05-07 11:36:31 +0300

    test

--- create/index.js
+++ create/index.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { create } from 'create-create-app';
+const path = require('path');
+const { create } = require('create-create-app');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const templateRoot = path.resolve(__dirname, '../');
 
 create('astro-starter-template-znu', {
-    templateRoot: path.resolve(__dirname, '../'),
+    templateRoot,
 });
